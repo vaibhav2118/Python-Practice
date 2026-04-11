587132017e23753312ea5f3a230d0b81f8f5e8e8
vaibhav2118 <vaibhavsalegave@gmail.com>
2026-04-11 20:23:25 +0530

    Add mathematical operations script

---
+++ Operations.py
@@ -0,0 +1,9 @@
+print("Lets do the mathyematical operations")
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+print("Sum of ",num1, "and" , num2, "is", num1+num2)
+print("Difference of ",num1,"and",num2,"is",num1-num2)
+print("Product of ",num1,"and",num2,"is",num1*num2)
+print("Division of ",num1,"and",num2,"is",num1/num2)
